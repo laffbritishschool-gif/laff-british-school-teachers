6e53d2bf39c2304bf0b0a8d4c0109c11c84d8b9f
laffbritishschool-gif <laffbritishschool@gmail.com>
2026-09-11 15:14:22 +0100

    Connect teacher portal to Supabase

---
+++ assets/supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://moqpmrhholbbhuedvbgg.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_IfSp9O5zUubH6rifFbfmZQ_DJttLC1f';
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
